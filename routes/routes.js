0566013317ce73cfd8530172c64f80a14c186d86
aaculp <aaculp@icloud.com>
2018-11-05 16:45:09 -0500

    Made routes, mirgrations and seed folders

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const placesRoutes = express.Router();
+
+const placesController = require('../controllers/controller');
+
+placesRoutes.get('/', placesController.index);
+placesRoutes.post('/', placesController.create);
+
+placesRoutes.get('/:id', placesController.show);
+placesRoutes.put('/:id', placesController.update);
+placesRoutes.delete('/:id', placesController.destroy);
+
+module.exports = placesRoutes;
